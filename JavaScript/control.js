5d8633be27f88be58c4a50148c8d7caa515399fb
hapo-quangtm <68210682+hapo-quangtm@users.noreply.github.com>
2020-08-12 08:35:09 +0700

    Add mess-feedback

--- JavaScript/control.js
+++ JavaScript/control.js
@@ -1,13 +1,43 @@
-$('.multi-item-carousel .item').each(function(){
-  var next = $(this).next();
-  if (!next.length) {
-    next = $(this).siblings(':first');
-  }
-  next.children(':first-child').clone().appendTo($(this));
+$(document).ready(function(){
+  $('#header-button').click(function() {
+    $('#icon').toggleClass('fa-times');
+    $('#icon').toggleClass('fa-bars');
+  });
   
-  if (next.next().length>0) {
-    next.next().children(':first-child').clone().appendTo($(this));
-  } else {
-    $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-  }
+  $('.fb-content').slick({
+    centerMode: false,
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    autoplay: true,
+    autoplaySpeed : 2000,
+    pauseOnFocus: true,
+    pauseOnHover: true,
+    prevArrow: $('.next-left'),
+    nextArrow: $('.next-right'),
+    
+    responsive: [
+      {
+        breakpoint: 980,
+        settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+        }
+      }
+    ]
+  });
 });
+
+function messBtn() {
+  var messbtns = document.getElementById("chat");
+  if (messbtns.style.display == "block") {
+    messbtns.style.display = "none";
+  } else {
+    messbtns.style.display = "block";
+  }
+}
+  
+function closeBtn() {
+  var closebtns = document.getElementById("chat");
+  closebtns.style.display = "none";
+}
